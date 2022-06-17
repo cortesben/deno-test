281938c419b616b93f2c0b7447843b41b438a5ae
Benjamin Cortes <cortes.ben@gmail.com>
2022-06-16 21:40:46 -0400

    notes and added a broken test

--- src/test.ts
+++ src/test.ts
@@ -2,5 +2,6 @@ import { assertEquals } from "https://deno.land/std@0.97.0/testing/asserts.ts";
 
 assertEquals("hello", "hello");
 assertEquals("world", "world");
+assertEquals("hello", "world");
 
 console.log("Asserted! ✓");
